027c25eeb493a57b4c41109d76759343aeedc2fb
lightrao <lightrao@hotmail.com>
2024-08-28 20:46:44 +0800

    Saving New Expenses

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:adding_interactivity_expense_tracker_app/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -57,6 +59,14 @@ class _NewExpenseState extends State<NewExpense> {
     }
 
     // use the entered data and create a new expense
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory,
+      ),
+    );
   }
 
   @override
